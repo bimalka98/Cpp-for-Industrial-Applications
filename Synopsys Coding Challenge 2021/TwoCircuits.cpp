4b751c25ab31b73bea592596283770bd82390b1b
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-12-03 20:26:03 +0530

    Tow circuits. best 63

--- Synopsys Coding Challenge 2021/TwoCircuits.cpp
+++ Synopsys Coding Challenge 2021/TwoCircuits.cpp
@@ -5,56 +5,116 @@ Let's name two ciructs as CircuitA and CircuitB. CircuitA's output is poA (po de
 a. Equal for all the possible inputs i.e (for all input combinations poA == poB)
 b. Completely inverted for all the possible inputs (for all input combinations poA == ~ poB)
 c. None of the above
-Each circuit is made up using following logic functions.
-a. Two input AND, NAND, XOR, XNOR, OR, NOR gates
-b. one input NOT gate
-c. One input BUF gate (just a buffer)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int evaluate_expression(string expression, int input1, int input2)
-{
+int evaluate(string oprtr, int input1, int input2){
     int result = 0;
-    if (expression == "AND")
-    {
-        result = input1 & input2;
-    }
-    else if (expression == "NAND")
-    {
-        result = ~(input1 & input2);
-    }
-    else if (expression == "XOR")
-    {
-        result = input1 ^ input2;
-    }
-    else if (expression == "XNOR")
-    {
-        result = ~(input1 ^ input2);
-    }
-    else if (expression == "OR")
-    {
-        result = input1 | input2;
-    }
-    else if (expression == "NOR")
-    {
-        result = ~(input1 | input2);
-    }
-    else if (expression == "NOT")
-    {
-        result = ~input1;
-    }
-    else if (expression == "BUF")
-    {
-        result = input1;
-    }
+    if (oprtr == "and") result = input1 & input2;
+    else if (oprtr == "nand") result = ~(input1 & input2);
+    else if (oprtr == "xor") result = input1 ^ input2;
+    else if (oprtr == "xnor") result = ~(input1 ^ input2);
+    else if (oprtr == "or") result = input1 | input2;
+    else if (oprtr == "nor") result = ~(input1 | input2);
+    else if (oprtr == "not") result = ~input1;
+    else if (oprtr == "buf") result = input1;
     return result;
 }
 
-string simplify(vector<string> circuit){
+/*
+CircuitA
+t1 or pi0 pi1
+t2 or t1 pi2
+t3 or t2 pi3
+poA buf ~t3
+CircuitB
+new_n1 and ~pi0 ~pi1
+new_n2 and new_n1 ~pi2
+new_n3 and new_n2 ~pi3
+poB not new_n3
+*/
+int simplify(string cct, vector<string> circuit, bitset<20> number){
     // replece evrything by primary inputs
-    continue;
+    int ans = 0; // ans is the output of the circuit
+    // bitset<20> primary_inputs = number;
+    map<string, int> values;
+    for(int i =0; i <20; i++){
+        // concatenate  "pi" + i
+        string pi = "pi" + to_string(i);
+        values[pi] = number[i];
+    }
+    int index = 1;
+    while(index <= (int)circuit.size()){
+        //output-variable, logical-operation, operand1, operand2
+        string oprtr = circuit[index];
+        /*
+        Each circuit is made up using following logic functions.
+        a. Two input and, nand, xor, xnor, or, nor gates
+        b. one input not gate
+        c. One input not gate (just a buffer)
+        */
+        if(oprtr == "and" || oprtr == "nand" || oprtr == "xor" || oprtr == "xnor" || oprtr == "or" || oprtr == "nor"){
+            string output = circuit[index-1];
+            string operand1 = circuit[index+1];
+            string operand2 = circuit[index+2];
+            int value1, value2;
+            if(operand1[0] == '~'){
+                operand1 = operand1.substr(1);
+                value1 = ~values[operand1];
+            }else{
+                value1 = values[operand1];
+            }
+            if(operand2[0] == '~'){
+                operand2 = operand2.substr(1);
+                value2 = ~values[operand2];
+            }else{
+                value2 = values[operand2];
+            }
+            int result = evaluate(oprtr, value1, value2);
+            values[output] = abs(result);
+            circuit.erase(circuit.begin()+index-1, circuit.begin()+index+3);
+            index = 1;
+        }
+        else if(oprtr == "not"){
+            string output = circuit[index-1];
+            string operand1 = circuit[index+1];
+            int value1;
+            if(operand1[0] == '~'){
+                operand1 = operand1.substr(1);
+                value1 = ~values[operand1];
+            }else{
+                value1 = values[operand1];
+            }
+            int result = evaluate(oprtr, value1, 0);
+            values[output] = abs(result);
+            circuit.erase(circuit.begin()+index-1, circuit.begin()+index+2);
+            index = 1;
+        }
+        else if(oprtr == "buf"){
+            string output = circuit[index-1];
+            string operand1 = circuit[index+1];
+            int value1;
+            if(operand1[0] == '~'){
+                operand1 = operand1.substr(1);
+                value1 = ~values[operand1];
+            }else{
+                value1 = values[operand1];
+            }
+            int result = evaluate(oprtr, value1, 0);
+            values[output] = abs(result);
+            circuit.erase(circuit.begin()+index-1, circuit.begin()+index+2);
+            index = 1;
+        }
+    }
+    if(cct == "CircuitA"){
+        ans =  values["poA"];
+    }
+    else if(cct == "CircuitB"){
+        ans  = values["poB"];
+    }
+    return ans;
 }
 
 int main(){
@@ -90,10 +150,53 @@ int main(){
     }
     
     // print the elements of CircuitA
-    cout << "CircuitA: ";
-    for(auto i : CircuitA) cout << i << endl;
-    // print the elements of CircuitB
-    cout << "CircuitB: ";
-    for(auto i : CircuitB) cout << i << endl;
-    return 0;
+    // cout << "CircuitA: ";
+    // for(auto i : CircuitA) cout << i << endl;
+    // // print the elements of CircuitB
+    // cout << "CircuitB: ";
+    // for(auto i : CircuitB) cout << i << endl;
+    // ---------------------simplify--------------------
+    // get the simplified circuit
+    // int out1 = abs(simplify("CircuitA", CircuitA, bitset<20>(0)));
+    // int out2 = abs(simplify("CircuitB", CircuitB, bitset<20>(0)));
+    // // print the simplified circuit
+    // cout << "CircuitA: " << out1 << endl;
+    // cout << "CircuitB: " << out2 << endl;
+    /*
+    check whether output of these two circuits are
+    a. Equal for all the possible inputs i.e (for all input combinations poA == poB)
+    b. Completely inverted for all the possible inputs (for all input combinations poA == ~ poB)
+    c. None of the above
+    */
+    // check whether output of these two circuits are equal for all the possible inputs
+    bool identical = true;
+    bool inverted = true;
+    for(int i = n/4; i < 3*n/4; i++){
+        bitset<20> number(i);
+        int out1 = abs(simplify("CircuitA", CircuitA, number));
+        int out2 = abs(simplify("CircuitB", CircuitB, number));
+        if(out1 != out2){
+            identical = false;
+            break;
+        }
+    }
+    // check whether output of these two circuits are completely inverted for all the possible inputs
+    for(int i = n/4; i < 3*n/4; i++){
+        bitset<20> number(i);
+        int out1 = abs(simplify("CircuitA", CircuitA, number));
+        int out2 = abs(simplify("CircuitB", CircuitB, number));
+        if(out1 != ~out2){
+            inverted = false;
+            break;
+        }
+    }
+    if(identical){
+        cout << "Identical" << endl;
+    }
+    if(inverted){
+        cout << "Inverted" << endl;
+    }
+    if(!identical && !inverted){
+        cout << "None" << endl;
+    }
 }
